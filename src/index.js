74e759bc12397f3a19077f7b6125dbc06815e806
Roman Hyrych <roman.girich@gmail.com>
2022-04-06 22:38:49 +0300

    base set

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './sass/main.scss';
+
+import './js/fetch_api';
+import './js/render_popular';
+import './js/pagination';
+import './js/render_search';
+import './js/modal_details';
+import './js/render_service';
+import './js/genres_info';
+import './js/render_input';
+import './js/to_top_arrow';
+import './js/render_trailer';
+import './js/storage';
+import './js/library';
