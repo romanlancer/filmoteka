9d5eaaf149b22e87dd19a67e4847f4e1e496bbbe
MViktoriia <mihaleva.victoria@gmail.com>
2022-04-08 03:45:05 +0300

    add filmCard template and render function

---
+++ src/js/filmCard.js
@@ -0,0 +1,122 @@
+import { createElement } from './createElement'
+import MoviesApiService from './fetch_api';
+import { genresInfo } from './genres_info'
+
+const movieApiServies = new MoviesApiService;
+const containerEl = document.querySelector('.cards__list');
+
+
+export const filmCard = filmData => {
+    const { id: filmId, poster_path: posterPath, original_title: originalTitle, overview, genre_ids: genreIds, release_date: releaseDate, vote_average: voteAverage } = filmData;
+
+    const originalTitleToUpperCase = originalTitle.toUpperCase();
+    const releaseYear = releaseDate.slice(0, 4);
+    // const genresNames = getGenresNames(genreIds)
+
+            
+    const filmPosterElem = createElement('img', {
+    class: 'cards__item-poster',
+      src: `https://image.tmdb.org/t/p/w500${ posterPath }`,
+    alt: 'film poster',
+    });
+
+    const filmPosterWrapperElem = createElement(
+    'div',
+    {
+      class: 'cards__item-poster-wrapper',
+    },
+    filmPosterElem,
+  );
+    
+    const filmTitleElem = createElement(
+    'h2',
+    {
+      class: 'cards__item-title',
+    },
+    originalTitleToUpperCase,
+  );
+
+  const filmGenresElem = createElement(
+    'p',
+    {
+      class: 'cards__item-genres',
+    },
+      getGenresNames(genreIds),
+    );
+    
+    const filmReleaseElem = createElement(
+    'p',
+    {
+      class: 'cards__item-release-date',
+    },
+      releaseYear,
+    );
+
+    const filmRateElem = createElement(
+    'span',
+    {
+      class: 'cards__item-vote-average',
+    },
+      voteAverage,
+    );
+
+    const filmDataElem = createElement(
+    'div',
+    {
+      class: 'cards__item-info',
+    },
+      [filmGenresElem, filmReleaseElem, filmRateElem],
+    );
+
+    const filmLinkElem = createElement(
+    'a',
+    {
+      class: 'cards__link',
+    },
+      [filmPosterWrapperElem],
+    );
+
+
+  const filmCardElem = createElement(
+    'li',
+    {
+      class: 'cards__item',
+    },
+    [filmLinkElem, filmTitleElem, filmDataElem]
+    );
+    
+    
+  return filmCardElem;
+};
+
+function getGenresNames(genreIds) {
+    let genresNamesArray = [];
+    for (const genreId of genreIds) {
+        genresInfo.map(genreInfo => {
+            if (genreInfo.id === genreId) {
+                genresNamesArray.push(genreInfo.name);
+            }
+        })      
+    }
+    if (genresNamesArray.length > 3) {
+        const genresNamesArrayShort = (genresNamesArray.slice(0, 2)).toString() + ', Other';
+        return genresNamesArrayShort;
+    }
+   
+    return genresNamesArray.toString();
+};
+
+
+movieApiServies.getPopularFilms().then(response => {
+    console.log(response.results);
+    renderFilmList(response.results);
+    
+}).catch(error => console.log(error));
+
+
+function renderFilmList (filmList) {
+  containerEl.innerHTML = '';
+  const filmsNodeList = filmList.map(film => filmCard(film));
+  containerEl.append(...filmsNodeList);
+};
+
---
+++ src/js/genres_info.js
@@ -0,0 +1,21 @@
+// import axios from 'axios';
+// const BASE_URL = 'https://api.themoviedb.org';
+// const API_KEY = 'e236468c654efffdf9704cd975a74a96';
+
+// export async function getGenresInfo() {
+//     try {
+//       const url = `${BASE_URL}/3/genre/movie/list?api_key=${API_KEY}&language=en-US`;
+//         const response = await axios.get(url);
+//         console.log(response.data.genres);
+//       return response.data.genres;
+//     } catch (error) {
+//       return error;
+//     }
+// }
+
+export const genresInfo = [{ "id": 28, "name": "Action" }, { "id": 12, "name": "Adventure" }, { "id": 16, "name": "Animation" }, { "id": 35, "name": "Comedy" }, { "id": 80, "name": "Crime" }, { "id": 99, "name": "Documentary" }, { "id": 18, "name": "Drama" }, { "id": 10751, "name": "Family" }, { "id": 14, "name": "Fantasy" }, { "id": 36, "name": "History" }, { "id": 27, "name": "Horror" }, { "id": 10402, "name": "Music" }, { "id": 9648, "name": "Mystery" }, { "id": 10749, "name": "Romance" }, { "id": 878, "name": "Science Fiction" }, { "id": 10770, "name": "TV Movie" }, { "id": 53, "name": "Thriller" }, { "id": 10752, "name": "War" }, { "id": 37, "name": "Western" }];
+
+
+
+  
+
