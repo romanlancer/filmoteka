71cdb3fe7022ef1ac3fa19344ce90301bf053f7a
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-04-10 15:35:48 +0300

    add developer

---
+++ src/js/render_footer_modal.js
@@ -0,0 +1,32 @@
+import users from './users';
+// import studentTpl from '../templates/student.hbs';
+
+const slide = document.querySelector('.swiper-wrapper');
+const userMarkup = renderUserCard(users);
+
+// function renderUserCard(users) {
+//   const marcup = studentTpl(users);
+//   // console.log('~ marcup', marcup);
+//   slide.insertAdjacentHTML('beforeend', marcup);
+// }
+
+function renderUserCard(data) {
+  return users
+    .map(({ name, role, img, discription }) => {
+      return `
+      <div class="swiper-slide ">
+        <img class="student-foto"
+          src=${img} alt=${name} />
+        <div class="student__card">
+          <h3 class="student__name">${name}</h3>
+          <p class="student__role">${role}</p>
+          <p class="student__about">
+            ${discription}
+          </p>
+        </div>
+      </div>`;
+    })
+    .join('');
+}
+
+slide.insertAdjacentHTML('beforeend', userMarkup);
---
+++ src/js/users.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    id: '1',
+    name: 'Aleksandr Serebro',
+    role: 'Developer',
+    img: './images/serebro.jpg',
+    discription: 'orem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+  {
+    id: '2',
+    name: 'Aleksandr Serebro',
+    role: 'Developer',
+    img: '../images/serebro.jpg',
+    discription: 'orem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+];
+
+export default users;
