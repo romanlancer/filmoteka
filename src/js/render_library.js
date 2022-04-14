b9c83d95fe25f4ed8d99a3b70022dd31fa8f1371
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2022-04-14 15:15:29 +0300

    add mediaquery

--- src/js/render_library.js
+++ src/js/render_library.js
@@ -4,17 +4,74 @@ import Pagination from './pagination';
 import { renderFilmList, addFilmListToContainer } from './filmCard';
 import { paginationChangeHandler, loadMoreChangeHandler, smoothScroll } from './pagination';
 import { getFromStorage } from './storage';
+import debounce from 'debounce';
 
-let watchedFilms = [];
 let currentPageWatched = 1;
 let currentPageQueue = 1;
-const cardsPerPage = 3;
+let cardsPerPage = 1;
+
+if (window.matchMedia("(min-width: 320px)").matches) {
+  cardsPerPage = 4;
+}
+if (window.matchMedia("(min-width: 768px)").matches) {
+  cardsPerPage = 6;
+}
+if (window.matchMedia("(min-width: 1024px)").matches) {
+  cardsPerPage = 9;
+}
+
+window.addEventListener("resize", debounce(resizeWindowHandler, 100));
+
+function resizeWindowHandler(event) {
+  const windowWidth = window.innerWidth;
+  if (windowWidth <= 768) {
+    cardsPerPage = 4;
+  }
+    if (windowWidth > 768 && windowWidth <= 1024) {
+    cardsPerPage = 6;
+  }
+  if (windowWidth >= 1024) {
+    cardsPerPage = 9;
+  }
+  
+}
+
+// const mediaMobileRef = window.matchMedia("(min-width: 320px)");
+// const mediaTabletRef = window.matchMedia("(min-width: 768px)");
+// const mediaDesktopRef = window.matchMedia("(min-width: 1024px)");
+
+// mediaMobileRef.addEventListener('change', screenMobileHandler);
+// mediaTabletRef.addEventListener('change', screenTabletHandler);
+// mediaDesktopRef.addEventListener('change', screenDesktopHandler);
+
+// function screenMobileHandler(event) {
+//   if(event.matches)console.log('mobile');
+// }
+// function screenTabletHandler(event) {
+//   if(event.matches)console.log('tablet');
+// }
+// function screenDesktopHandler(event) {
+//   if(event.matches)console.log('desktop');
+// }
+// if (window.matchMedia("(min-width: 320px)").matches) {
+//   cardsPerPage = 4;
+//   console.log('screen 320');
+// }
+// if (window.matchMedia("(min-width: 768px)").matches) {
+//   cardsPerPage = 6;
+//    console.log('screen 768');
+// }
+// if (window.matchMedia("(min-width: 1024px)").matches) {
+//   cardsPerPage = 9;
+//    console.log('screen 1024');
+// }
+
 
 const moviePaginationForWatched = new Pagination({
   initialPage: 1,
   total: 1,
   onChange(value) {
-    handlePageChangeWatched(value, cardsPerPage, watchedFilms);
+    handlePageChangeWatched(value, cardsPerPage);
   },
 });
 
@@ -26,15 +83,7 @@ const moviePaginationForQueue = new Pagination({
   },
 });
 
-watchedFilms = getFromStorage('dataFilmsByWatched');
 
-if (!watchedFilms) watchedFilms = [];
-
-watchedFilms.forEach((data) => {
-  data.genre_ids = data.genres.map((data) => {
-    return data.id;
-  })
-});
 
 export function renderWatched(page) {
   if(page) moviePaginationForWatched.currentPage = page;
@@ -49,15 +98,27 @@ export function renderQueue(page) {
 
 
 
-function handlePageChangeWatched(page, elPerPage, arrFilms) {
+function handlePageChangeWatched(page, elPerPage) {
   currentPageWatched = page;
-  const totalPages = Math.ceil(arrFilms.length / elPerPage);
+  const watchedFilms = getFromStorage('dataFilmsByWatched') ?? [];
+
+  watchedFilms.forEach((data) => {
+    data.genre_ids = data.genres.map((data) => {
+      return data.id;
+    })
+  });
+  const totalPages = Math.ceil(watchedFilms.length / elPerPage);
   moviePaginationForWatched.total = totalPages;
-
-  const FilmsForRender = arrFilms.slice((page - 1) * elPerPage, page * elPerPage);
-
-  renderFilmList(FilmsForRender);
-  moviePaginationForWatched.renderPaginationDisabled(
+  const FilmsForRender = watchedFilms.slice((page - 1) * elPerPage, page * elPerPage);
+  Loading.hourglass({
+    cssAnimationDuration: 400,
+    svgSize: '150px',
+    svgColor: '#ff6b01',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+  });
+  setTimeout(() => {
+    renderFilmList(FilmsForRender);
+    moviePaginationForWatched.renderPaginationDisabled(
       document.querySelector('.pagination-list'),
       totalPages,
       page,
@@ -68,39 +129,57 @@ function handlePageChangeWatched(page, elPerPage, arrFilms) {
       getFromStorage('language'),
     );
   paginationChangeHandler(onPaginationWatchedHandler);
-  loadMoreChangeHandler(onLoadMoreWatchedHandler);
+    loadMoreChangeHandler(onLoadMoreWatchedHandler);
+    Loading.remove();
+  }, 300);
+
 }
 
 function onLoadMoreWatchedHandler(event) {
   currentPageWatched += 1;
+
+  const watchedFilms = getFromStorage('dataFilmsByWatched') ?? [];
+  watchedFilms.forEach((data) => {
+    data.genre_ids = data.genres.map((data) => {
+      return data.id;
+    })
+  });
   const totalPages = Math.ceil(watchedFilms.length / cardsPerPage);
-
   const FilmsForRender = watchedFilms.slice((currentPageWatched - 1) * cardsPerPage, currentPageWatched * cardsPerPage);
-
-  addFilmListToContainer(FilmsForRender);
-  moviePaginationForWatched.renderPaginationDisabled(
-      document.querySelector('.pagination-list'),
-      totalPages,
-      currentPageWatched,
-  );
-  moviePaginationForWatched.renderPaginationLoadMore(
-      document.querySelector('.pagination'),
-      currentPageWatched,
-      getFromStorage('language'),
+  Loading.hourglass({
+    cssAnimationDuration: 400,
+    svgSize: '150px',
+    svgColor: '#ff6b01',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+  });
+  setTimeout(() => {
+    addFilmListToContainer(FilmsForRender);
+    moviePaginationForWatched.renderPaginationDisabled(
+        document.querySelector('.pagination-list'),
+        totalPages,
+        currentPageWatched,
     );
-  loadMoreChangeHandler(onLoadMoreWatchedHandler);
+    moviePaginationForWatched.renderPaginationLoadMore(
+        document.querySelector('.pagination'),
+        currentPageWatched,
+        getFromStorage('language'),
+    );
+    loadMoreChangeHandler(onLoadMoreWatchedHandler);  
 
-  for (let i = 0; i < document.querySelector('.pagination-list').childNodes.length; i += 1) {
-      const number = parseInt(
-        document.querySelector('.pagination-list').childNodes[i].firstChild.textContent,
-      );
-      if (number >= moviePaginationForWatched.currentPage && number <= currentPageWatched) {
-        if (document.querySelector('.pagination-list').childNodes[i].classList.contains('active')) {
-          document.querySelector('.pagination-list').childNodes[i].classList.remove('active');
+    for (let i = 0; i < document.querySelector('.pagination-list').childNodes.length; i += 1) {
+        const number = parseInt(
+          document.querySelector('.pagination-list').childNodes[i].firstChild.textContent,
+        );
+        if (number >= moviePaginationForWatched.currentPage && number <= currentPageWatched) {
+          if (document.querySelector('.pagination-list').childNodes[i].classList.contains('active')) {
+            document.querySelector('.pagination-list').childNodes[i].classList.remove('active');
+          }
+          document.querySelector('.pagination-list').childNodes[i].classList.add('loaded');
         }
-        document.querySelector('.pagination-list').childNodes[i].classList.add('loaded');
-      }
     }
+    Loading.remove();
+  }, 300);
+  
 }
 
 function onPaginationWatchedHandler(event) {
