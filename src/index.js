d6b0c8af87b5c20e8992832ed32b4ea47c887e9f
Roman Hyrych <roman.girich@gmail.com>
2022-04-15 18:07:26 +0300

    added fontawesome library for icons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './sass/main.scss';
+import '../node_modules/@fortawesome/fontawesome-free/js/solid.min';
+import '../node_modules/@fortawesome/fontawesome-free/js/brands.min';
+import '../node_modules/@fortawesome/fontawesome-free/js/fontawesome.min';
 import './js/fetch_api';
 import './js/home';
 import './js/render_popular';
