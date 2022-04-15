70e8677e3251d8d44210b482d3301c698f52ae71
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-04-15 15:24:44 +0300

    add img and overflow

--- src/js/modal_footer.js
+++ src/js/modal_footer.js
@@ -3,6 +3,7 @@ const refs = {
   closeModalBtn: document.querySelector('[data-footer-close]'),
   modal: document.querySelector('[data-modal-footer]'),
   backdrop: document.querySelector('.js-backdrop'),
+  addBodyClass: document.querySelector('body'),
 };
 
 refs.openModalLink.addEventListener('click', openModal);
@@ -13,18 +14,21 @@ function onClickEscape(event) {
   if (event.key === 'Escape') {
     console.log(event.key);
     refs.modal.classList.add('is-hidden');
+    refs.addBodyClass.classList.remove('modal-open');
     document.removeEventListener('keydown', onClickEscape);
   }
 }
 
 function closeModal() {
   refs.modal.classList.add('is-hidden');
+  refs.addBodyClass.classList.remove('modal-open');
   document.removeEventListener('keydown', onClickEscape);
   // modal.innerHTML = '';
 }
 
 function openModal() {
   refs.modal.classList.remove('is-hidden');
+  refs.addBodyClass.classList.add('modal-open');
   document.addEventListener('keydown', onClickEscape);
 }
 
