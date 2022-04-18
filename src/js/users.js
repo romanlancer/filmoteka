cba5b66f0495315da41ca37ef7bfea91c20092d5
Roman Hyrych <roman.girich@gmail.com>
2022-04-18 21:12:15 +0300

    Update users.js

--- src/js/users.js
+++ src/js/users.js
@@ -9,7 +9,7 @@ export const users = [
     LN_Link:
       'https://www.linkedin.com/in/%D1%80%D0%BE%D0%BC%D0%B0%D0%BD-%D0%B3%D0%B8%D1%80%D0%B8%D1%87-1511bb226/',
     discription:
-      'Hell, my friends. I am a beginning FrontEnd developer.I am very excited at learning new things and technologies attached with FrontEnd. Already have some projects in my Portfolio on github.',
+      'Hello, my friends. I am a beginning FrontEnd developer.I am very excited at learning new things and technologies attached with FrontEnd. Already have some projects in my Portfolio on github.',
   },
   {
     id: '2',
